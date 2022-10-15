c407e8a18db6a18517a1b893186ad95a0022d210
xkendrickz <kendrickchandra128@gmail.com>
2022-10-15 20:25:40 +0700

    tambah regis

--- process/registerProcess.php
+++ process/registerProcess.php
@@ -11,30 +11,40 @@
         $name = $_POST['name'];
         
         $checkEmail = mysqli_query($con, "SELECT * FROM users WHERE email = '$email'") or
-             die(mysqli_error($con));
+            die(mysqli_error($con));
+        $checkUsername = mysqli_query($con, "SELECT * FROM users WHERE name = '$name'") or
+            die(mysqli_error($con));
 
         if(mysqli_num_rows($checkEmail) !=0){
             echo
-            '<script>
-            alert("Email already register");
-            window.location = "../page/registerPage.php"
-            </script>';
-    }else{
-        $query = mysqli_query($con, "INSERT INTO users(name, passwords, email) VALUES
-        ('$name', '$passwords', '$email')") or die(mysqli_error($con));
-
-        if($query){
+                '<script>
+                alert("Email already registered");
+                window.location = "../page/registerPage.php"
+                </script>';
+        }else if(mysqli_num_rows($checkUsername) !=0){
             echo
                 '<script>
-                alert("Register Success");
-                window.location = "../index.php"
+                alert("Username already registered");
+                window.location = "../page/registerPage.php"
                 </script>';
         }else{
-            echo
-                '<script>
-                alert("Register Failed");
-                </script>';
-        }        }
+            $query = mysqli_query($con, "INSERT INTO users(name, passwords, email) VALUES
+            ('$name', '$passwords', '$email')") or die(mysqli_error($con));
+
+            if($query){
+                echo
+                    '<script>
+                    alert("Register Success");
+                    window.location = "../index.php"
+                    </script>';
+            }else{
+                echo
+                    '<script>
+                    alert("Register Failed");
+                    window.location = "../page/registerPage.php"
+                    </script>';
+            }    
+        }
     }else{
         echo
             '<script>
