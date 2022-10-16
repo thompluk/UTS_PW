c19c27a895a4e45a2292c2c3eff8248dee274286
thompluk <thomasdany38@gmail.com>
2022-10-16 23:00:39 +0700

    nambah

--- process/deleteBukuProcess.php
+++ process/deleteBukuProcess.php
@@ -1,27 +1,28 @@
 <?php
-session_start();
-if(isset($_GET['id'])){
-    include ('../db.php');
-    $id = $_GET['id'];
-    
-    $queryDelete = mysqli_query($con, "DELETE FROM buku WHERE id='$id'") or
-    die(mysqli_error($con));
-    
-    if($queryDelete){
+    if(isset($_GET['id'])){
+        include ('../db.php');
+        session_start();
+        $id = $_GET['id'];
+        
+        $queryDelete = mysqli_query($con, "DELETE FROM buku WHERE id='$id'") or
+        die(mysqli_error($con));
+        
+        if($queryDelete){
+            echo
+            '<script>
+            alert("Delete Success"); 
+            window.location = "../page/listBukuPage.php"
+            </script>';
+        }else{
+            echo
+            '<script>
+            alert("Delete Failed"); window.location = "../page/listBukuPage.php"
+            </script>';
+        }
+    }else {
         echo
         '<script>
-        alert("Delete Success"); window.location = "../page/listBukuPage.php"
-        </script>';
-    }else{
-        echo
-        '<script>
-        alert("Delete Failed"); window.location = "../page/listBukuPage.php"
+        window.history.back()
         </script>';
     }
-}else {
-    echo
-    '<script>
-    window.history.back()
-    </script>';
-}
 ?>
