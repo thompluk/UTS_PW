113ca4a161edcb2c9c68a82992198254b57f83f3
valentVasti <valentvasti2114@gmail.com>
2022-10-18 12:00:27 +0700

    pesan tempat di user doang

--- component/sidebar.php
+++ component/sidebar.php
@@ -106,11 +106,17 @@ echo '
                                         <i class="fa fa-person"></i>
                                         <a href="./pegawaiPage.php" style="font-weight:500">Pegawai</a>
                                     </div>';
+                                }else{
+                                    echo'
+                                    <div class="content-menu" >
+                                        <i class="fa fa-table"></i>
+                                            <a href="./pesanTempatPage.php" style="font-weight:500">Pesan Tempat</a>
+                                    </div>';       
                                 }
                                 echo'
                                 <div class="content-menu" >
-                                    <i class="fa fa-table"></i>
-                                    <a href="./pesanTempatPage.php" style="font-weight:500">Pesan Tempat</a>
+                                    <i class="fa fa-user"></i>
+                                        <a href="./profileUsersPage.php" style="font-weight:500">Profile</a>
                                 </div>
                                 <div class="content-menu" >
                                     <i class="fa fa-arrow-right-from-bracket"></i>
